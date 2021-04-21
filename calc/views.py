25a3fb6cd6c4511ced537d0431e459390c22b43d
Tipu Sultan Eiko <tipusultan50@gmail.com>
2021-04-21 20:31:02 +0600

    addition and render

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'new': 'news'})
+
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    return render(request, 'result.html', {'result': val1 + val2})
